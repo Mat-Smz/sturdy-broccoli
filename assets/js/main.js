ba83c2a9098bbb2236089966e720b437983b8d2e
Mat-Smz <59562144+Mat-Smz@users.noreply.github.com>
2020-01-31 14:15:21 +0100

    First Stuff

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.getElementById("btn3").addEventListener("click", () => {
+//     document.querySelector(".modal").classList.toggle("hidden");
+//   });
+// });
+
+const switchModal = () => {
+  console.log("openModal");
+  document.querySelector(".modal").classList.toggle("hidden");
+};
